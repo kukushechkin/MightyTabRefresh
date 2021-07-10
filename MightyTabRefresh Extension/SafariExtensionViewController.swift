3120834ce8a69f2eb566ee905fcd3f289371d3db
Vladimir Kukushkin <vladimir.kukushkin@f-secure.com>
2021-07-10 18:30:40 +0300

    Initial Commit

---
+++ MightyTabRefresh Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  MightyTabRefresh Extension
+//
+//  Created by Kukushkin, Vladimir on 10.7.2021.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
---
+++ MightyTabRefresh/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MightyTabRefresh
+//
+//  Created by Kukushkin, Vladimir on 10.7.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
