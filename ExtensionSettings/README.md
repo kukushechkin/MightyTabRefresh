1111128a93567855cce28f8c9a54bae5825aa8a3
kukushechkin <kukushechkin@mac.com>
2021-07-19 23:56:20 +0300

    add settings

---
+++ ExtensionSettings/README.md
@@ -0,0 +1,3 @@
+# ExtensionSettings
+
+A description of this package.
---
+++ ExtensionSettings/Sources/ExtensionSettings/ExtensionSettings.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public struct Rule: Codable {
+    public let enabled: Bool
+    public let pattern: String
+    public let refreshInterval: TimeInterval
+    
+    public init(enabled: Bool, pattern: String, refreshInterval: TimeInterval) {
+        self.enabled = enabled
+        self.pattern = pattern
+        self.refreshInterval = refreshInterval
+    }
+}
+
+/// Used to transfer urls and refresh times from the app to the extension.
+public struct ExtensionSettings: Codable {
+    public static let settingsMessageName = "com.kukushechkin.MightyTabRefresh.settingsMessage"
+    public static let settingsMessageKey = "com.kukushechkin.MightyTabRefresh.settings"
+    
+    public let rules: [Rule]
+    
+    public init(rules: [Rule]) {
+        self.rules = rules
+    }
+}
+
+// Safari App Extension protocol dictionary encoding
+public extension ExtensionSettings {
+    init?(from json: Any) {
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json, options: []),
+              let decoded = try? JSONDecoder().decode(ExtensionSettings.self, from: jsonData) else {
+            return nil
+        }
+        self = decoded
+    }
+    
+    func encode() throws -> Any {
+        let jsonData = try JSONEncoder().encode(self)
+        return try JSONSerialization.jsonObject(with: jsonData, options: .allowFragments)
+    }
+}
